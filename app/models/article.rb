36e6b88322178baf8e09cdb84b9c62917fdfea5c
Andrew Faircloth <andrewkfaircloth@gmail.com>
2016-02-09 12:25:11 -0500

    removed join tables

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  has_and_belongs_to_many :tags,  :join_table => "articles_tags"
+  # has_and_belongs_to_many :tags,  :join_table => "articles_tags"
 
   validates :title, presence: true,
                       length: { minimum: 2 }
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  has_and_belongs_to_many :articles, :join_table => "articles_tags"
+  # has_and_belongs_to_many :articles, :join_table => "articles_tags"
 
   has_many :articles, through: :articles_tags
 
